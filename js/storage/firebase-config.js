d8f26a3b5a3a254667c7c5f980d3f2f162cec483
정아랑 <braverygin@gmail.com>
2026-07-05 22:18:26 +0900

    TripDesk V1.4 Stable Rebuilt

---
+++ js/storage/firebase-config.js
@@ -0,0 +1,8 @@
+window.TRIPDESK_FIREBASE_CONFIG = {
+  apiKey: "여기에_apiKey",
+  authDomain: "tripdesk-4fa90.firebaseapp.com",
+  projectId: "tripdesk-4fa90",
+  storageBucket: "tripdesk-4fa90.firebasestorage.app",
+  messagingSenderId: "여기에_messagingSenderId",
+  appId: "여기에_appId"
+};
